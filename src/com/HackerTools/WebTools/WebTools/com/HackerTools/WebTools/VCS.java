089e8f9cc2bd2dcb65c2758bfce6cad4460b72b0
XiaoLi8848 <45089374+xiaoli8848@users.noreply.github.com>
2020-08-14 17:42:44 +0800

    添加支持的漏洞

--- src/com/HackerTools/WebTools/WebTools/com/HackerTools/WebTools/VCS.java
+++ src/com/HackerTools/WebTools/WebTools/com/HackerTools/WebTools/VCS.java
@@ -1,19 +1,20 @@
 package com.HackerTools.WebTools.WebTools.com.HackerTools.WebTools;
+
 import com.HackerTools.WebTools.WebToolsAPI;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class PHPInfo {
+public class VCS {
     public static boolean Judge(WebToolsAPI API){
         try {
-            URL url1 = new URL(API.url_now + "phpinfo.php");
-            URL url2 = new URL(API.url_now + "test.php");
+            URL url1 = new URL(API.url_now + "CVS/Entriesp");
+            URL url2 = new URL(API.url_now + ".svn/entriesp");
             if(API.exists(url1.toString()) || API.exists(url2.toString())){
-                API.writeLog(1,"发现PHPInfo漏洞");
+                API.writeLog(1,"发现VCS漏洞");
                 return true;
             }else{
-                API.writeLog(1,"未发现PHPInfo漏洞");
+                API.writeLog(1,"未发现VCS漏洞");
                 return false;
             }
         } catch (MalformedURLException e) {
@@ -23,9 +24,9 @@ public class PHPInfo {
     }
 
     public static boolean Attack(WebToolsAPI API){
-        API.openWebsite(java.net.URI.create(API.url_now + "phpinfo.php"));
-        API.openWebsite(java.net.URI.create(API.url_now + "test.php"));
-        API.writeLog(1,"已尝试攻击PHPInfo漏洞");
+        API.openWebsite(java.net.URI.create(API.url_now + "CVS/Entriesp"));
+        API.openWebsite(java.net.URI.create(API.url_now + ".svn/entriesp"));
+        API.writeLog(1,"已尝试攻击VCS漏洞");
         return true;
     }
 }
